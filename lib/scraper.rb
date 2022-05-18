b165277bdded51c64e50608bec776c5fe321c921
Merle Tyson Mills <tysonmills5@gmail.com>
2022-05-18 13:41:04 -0500

    commit tree 934722ac302ddaa176aeaad2a12ed8b784bdc3d5

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = URI.open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+doc.css(".container-1cfI6E")
